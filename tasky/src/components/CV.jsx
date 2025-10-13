b3af0e23cd6f2fe99c59a13703bb85d7cfffd1db
Will Gusackova <147726836+WillGusackov@users.noreply.github.com>
2025-10-13 22:40:06 +0100

    Update CV.jsx

--- tasky/src/components/CV.jsx
+++ tasky/src/components/CV.jsx
@@ -10,9 +10,9 @@ function CV() {
     <section className="card">
       <h2 className="title">CV</h2>
       <p>
-        <a href="https://willgusackov.github.io/CV.pdf" target='_blank' rel="noopener noreferrer" className="cv_link">
-          View My CV (PDF)
-        </a>
+       <a href={`${import.meta.env.BASE_URL}CV.pdf`} target="_blank" rel="noopener noreferrer">
+  View My CV (PDF)
+</a>
       </p>
     </section>
   );
